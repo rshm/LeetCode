8241b5a9eac1353dd2c022d2594a93173121d23a
rshm <rshm@users.noreply.github.com>
2022-10-12 10:02:55 +0200

    Time: 0 ms (100.00%), Space: 41.9 MB (63.51%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.java
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -1,23 +1,20 @@
 class Solution {
     public boolean isSubsequence(String s, String t) {
        
-        if(s.length() == 0 && t.length() == 0)
+       if(s.length() == 0 && t.length() == 0)
             return true;
-         else if(t.length() ==0)
+       else if(t.length() ==0)
             return false;
-                return isSub(s,t);
+             int j = -1;
+        
+       for(int i = 0 ;i <s.length();i++){
+           j = t.indexOf(s.charAt(i) , j+1);
+           if(j == -1)
+               return false;
+       } 
+        return true;
          
     }
     
-    public boolean isSub(String small, String large){
-        int j = -1;
-       for(int i = 0 ;i <small.length();i++){
-           j = large.indexOf(small.charAt(i) , j+1);
-           
-           if(j == -1)
-               return false;
-           
-       } 
-        return true;
-    }
+  
 }
