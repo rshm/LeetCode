358985e8830533ffbd03351a7fcd36990f132049
rshm <rshm@users.noreply.github.com>
2022-10-19 09:53:36 +0200

    Time: 1 ms (51.93%), Space: 40.7 MB (85.36%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -1,6 +1,6 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-        String split[] = s.split(" ");
+        String split[] = s.trim().split(" ");
         return split[split.length-1].length();
     }
     
