6143a60e4536f533bb1c1121c9a04e9e03bdd35b
rshm <rshm@users.noreply.github.com>
2022-10-19 12:37:31 +0200

    Time: 6 ms (89.82%), Space: 45.4 MB (61.65%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -5,15 +5,16 @@ class Solution {
         int v=0;
         for(int i = ch.length-1; i>=0; i--){
             switch(ch[i]){
-                case 'I' -> v = (i< ch.length-1 && (ch[i+1] == 'V' || ch[i+1] == 'X'))? -1: 1;
+                case 'I' -> v=1;
                 case 'V' -> v=5;
-                case 'X' -> v = (i< ch.length-1 && (ch[i+1] == 'L' || ch[i+1] == 'C'))? -10: 10;
+                case 'X' -> v = 10;
                 case 'L' -> v=50;
-                case 'C' -> v = (i< ch.length-1 && (ch[i+1] == 'D' || ch[i+1] == 'M'))? -100: 100;
+                case 'C' -> v = 100;
                 case 'D' -> v=500;
                 case 'M' -> v=1000;
             }
-            value+=v;
+            if( v*4 < value) value-=v;
+            else value+=v;
         }
         
         return value;
