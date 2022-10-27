1b158498c6bd6fbe14ab9ff53f1a34f2fe6e2dd2
rshm <rshm@users.noreply.github.com>
2022-10-27 12:51:10 +0200

    Time: 5 ms (61.43%), Space: 49.4 MB (51.89%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int jump(int[] nums) {
+        int goal = nums.length-1;
+     
+        int jump =0;
+        int furthest=0;
+        int l=0 , r=0; 
+       
+       while(r < nums.length-1){
+           for(int i=l;i<=r;i++){
+               furthest= Math.max(furthest, nums[i]+i);
+           }
+           l = r+1;
+           r = furthest;
+           jump++;
+          
+        }
+        
+        return jump;
+    }
+}
