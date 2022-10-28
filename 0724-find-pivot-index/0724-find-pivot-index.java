05000d3f5c1b02f35c69b3a12a38324c75924788
rshm <rshm@users.noreply.github.com>
2022-10-28 09:37:35 +0200

    Time: 2 ms (84.51%), Space: 51.8 MB (78.19%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.java
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -1,18 +1,17 @@
 class Solution {
     public int pivotIndex(int[] nums) {
-        int sum=0;
-        
+        int sum =0;
         for(int x:nums)
             sum+=x;
-        // check 0 index now
-        if((sum-nums[0]) == 0)
+        if(sum-nums[0] == 0)
             return 0;
         for(int i=1;i<nums.length;i++){
-            nums[i]+=nums[i-1];
-            if(nums[i-1] == (sum-nums[i]))
+            nums[i] += nums[i-1];
+            if(nums[i-1] == (sum - nums[i]))
                 return i;
         }
-        
         return -1;
     }
+
+    
 }
